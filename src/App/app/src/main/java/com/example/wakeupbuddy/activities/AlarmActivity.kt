095baeecb877f8ad96dbb41e1bead3c55c1608cc
Nicolai Kowol <tyrontv@gmx.de>
2022-12-04 15:23:54 +0900

    Implement snooze button + time and date view

--- src/App/app/src/main/java/com/example/wakeupbuddy/activities/AlarmActivity.kt
+++ src/App/app/src/main/java/com/example/wakeupbuddy/activities/AlarmActivity.kt
@@ -16,8 +16,16 @@ class AlarmActivity : AppCompatActivity() {
         binding = ActivityAlarmBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bDeactivateAlarm.setOnClickListener { view: View ->
-            println("Button clicked")
+        val curDate = Calendar.getInstance()
+        binding.alarmTime.text =
+            "${curDate.get(Calendar.HOUR_OF_DAY)}:${curDate.get(Calendar.MINUTE)}"
+        binding.alarmDate.text =
+            "${getWeekdayName(curDate.get(Calendar.DAY_OF_WEEK))}, ${curDate.get(Calendar.DAY_OF_MONTH)}. ${
+                getMonthName(curDate.get(Calendar.MONTH))
+            }"
+
+        binding.deactivateAlarmButton.setOnClickListener { view: View ->
+            println("Stop button clicked")
 
             val wkbApp = applicationContext as WakeUpBuddyApp
             val alarmManager = wkbApp.getMyAlarmManager()
@@ -30,6 +38,55 @@ class AlarmActivity : AppCompatActivity() {
             }
             finish()
         }
+
+        binding.snoozeAlarmButton.setOnClickListener { view: View ->
+            println("Snooze button clicked")
+
+            val wkbApp = applicationContext as WakeUpBuddyApp
+            val alarmManager = wkbApp.getMyAlarmManager()
+
+            val extras = intent.extras as Bundle
+            extras.getString("alarm_id")?.let { alarmManager.snoozeAlarm(it) }
+
+            if (isTaskRoot) {
+                startActivity(Intent(this, HomeActivity::class.java))
+            }
+            finish()
+
+        }
+    }
+
+    private fun getWeekdayName(day: Int): String {
+        var name = ""
+        when (day) {
+            1 -> name = "Sunday"
+            2 -> name = "Monday"
+            3 -> name = "Tuesday"
+            4 -> name = "Wednesday"
+            5 -> name = "Thursday"
+            6 -> name = "Friday"
+            7 -> name = "Saturday"
+        }
+        return name
+    }
+
+    private fun getMonthName(month: Int): String {
+        var name = ""
+        when (month) {
+            0 -> name = "January"
+            1 -> name = "February"
+            2 -> name = "March"
+            3 -> name = "April"
+            4 -> name = "May"
+            5 -> name = "June"
+            6 -> name = "July"
+            7 -> name = "August"
+            8 -> name = "September"
+            9 -> name = "October"
+            10 -> name = "November"
+            11 -> name = "December"
+        }
+        return name
     }
 
 }
